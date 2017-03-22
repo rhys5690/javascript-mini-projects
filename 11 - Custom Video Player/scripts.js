a68edd8cf51c250649b820873b6d65bf8332bcfd
rhys5690 <rhys5690@gmail.com>
2017-03-22 16:40:34 +1100

    .

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,6 +7,11 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.full-screen');
+const body = document.querySelector('body');
+console.log(player.clientWidth);
+console.log(player.clientHeight);
+console.log(body.clientWidth);
 
 //Build our functions
 
@@ -31,6 +36,25 @@ function changeVolume() {
   video[this.name] = this.value;
 }
 
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function changeProgressBar(e) {
+  const time = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = time;
+}
+
+function makeFullScreen() {
+  // const ratio = player.clientWidth / player.clientHeight;
+
+  const newWidth = body.clientWidth;
+  const newHeight = body.clientHeight;
+
+  player.clientWidth = newWidth;
+  player.clientHeight = 0.5 * newWidth;
+}
 
 //Hook up the event listeners
 
@@ -40,3 +64,12 @@ video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 skipButtons.forEach(button => button.addEventListener("click", skip));
 ranges.forEach(slider => slider.addEventListener("change", changeVolume));
+video.addEventListener("timeupdate", updateProgress);
+
+let mouseDown = false;
+
+progress.addEventListener("mousemove", (e) => mouseDown && changeProgressBar(e));
+progress.addEventListener("click", changeProgressBar);
+progress.addEventListener("mousedown", () => mouseDown = true);
+progress.addEventListener("mouseup", () => mouseDown = false);
+fullScreen.addEventListener("click", makeFullScreen);
